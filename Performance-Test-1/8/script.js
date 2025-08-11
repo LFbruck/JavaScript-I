afc43a0d1adf555ae315852696f39f3d056b6841
Luiz Filipe Bruck Reis Mattos <133892887+LFbruck@users.noreply.github.com>
2025-08-11 20:35:43 -0300

    Resolved

---
+++ Performance-Test-1/8/script.js
@@ -0,0 +1,7 @@
+const salarios = [1234, 14000, 3358, 6223];
+let soma = 0;
+let media;
+
+soma = salarios[0] + salarios[1] + salarios[2] + salarios[3];
+media = soma / salarios.length;
+console.log(media);
