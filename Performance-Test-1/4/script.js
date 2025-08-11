a29bd475560ab0c0925fd10d0f04d2da3b9a6c0f
Luiz Filipe Bruck Reis Mattos <133892887+LFbruck@users.noreply.github.com>
2025-08-11 20:28:18 -0300

    Resolved

---
+++ Performance-Test-1/4/script.js
@@ -0,0 +1,17 @@
+const paises = [];
+paises.push("brasil", " india", "espanha", "Alemanha", "Espanha");
+console.log(paises);
+console.log(paises.length);
+paises[1] = "italia";
+console.log(paises);
+
+paises.push("Finlandia");
+console.log(paises);
+delete paises[2];
+console.log(paises);
+
+paises.pop();
+console.log(paises);
+
+paises.push("Veneza", "Estados Unidos");
+console.log(paises);
