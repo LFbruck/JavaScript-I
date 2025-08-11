ff60dd546306bd47565c74391ecfa7d1a995d43b
Luiz Filipe Bruck Reis Mattos <133892887+LFbruck@users.noreply.github.com>
2025-08-11 20:24:59 -0300

    Creating Script.js

---
+++ Performance-Test-1/2/script.js
@@ -0,0 +1,11 @@
+const paises = [];
+paises.push("brasil", " india", "espanha", "Alemanha", "Espanha");
+console.log(paises);
+console.log(paises.length);
+paises[1] = "italia";
+console.log(paises);
+
+paises.push("Finlandia");
+console.log(paises);
+delete paises[2];
+console.log(paises);
